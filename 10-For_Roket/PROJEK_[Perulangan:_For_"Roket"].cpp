89d56cb028487e6ca1b6a1501d1a1a4b6083777a
Mite <daviddwiwahyudi626@gmail.com>
2025-12-31 12:52:25 +0700

    perulangan for

---
+++ 10-For_Roket/PROJEK_[Perulangan:_For_"Roket"].cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Selanat Datang di Program Perulangan: For 'Roket'\nSilahkan Ikuti Program Berikut.\n"<<endl;
+    for(int i=10;i>0;i--){
+        cout<<i<<endl;
+    }
+    cout<<"MELUNCUR!";
+    
+    
+    return 0;
+}
